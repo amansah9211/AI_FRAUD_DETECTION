f4f46aa57323a3e21f6960cc89994fce1ccc62c7
Aman <aman169.kumar25b@iiitg.ac.in>
2026-08-23 16:22:04 +0530

    updated js file with deployed backend link

--- Frontend/AI_Fraud.js
+++ Frontend/AI_Fraud.js
@@ -131,7 +131,7 @@ console.log("Sending to backend:", data);
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("http://127.0.https://ai-fraud-detection-6lal.onrender.com.1:8000/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
